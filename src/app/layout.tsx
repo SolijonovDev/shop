d1a1758d5a04ee0d729bfd04920a213c975959d0
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-10-18 13:59:37 +0500

    Mui added to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,42 @@
+'use client';
+import ThemeRegistry from '@/theme/theme';
 import Footer from '@/components/footer';
 import './globals.css';
 import type { Metadata } from 'next';
 import Navbar from '@/components/layout/Navbar';
+import { styled } from '@mui/material/styles';
 
-export const metadata: Metadata = {
-  title: 'Home page',
-  description: 'Developer by Solijonov Qodirjon',
-  authors: [{ name: 'Solijonov Qodirjon', url: 'https://solijonov.vercel.app/' }],
-};
+const StyledMain = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  background: 'blue',
+}));
+
+const Wrapper = styled('div')(({ theme }) => ({
+  display: 'flex',
+  minHeight: '100vh',
+  flexDirection: 'column',
+}));
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </head>
       <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+        <ThemeRegistry>
+          <Wrapper>
+            <Navbar />
+            <StyledMain>{children}</StyledMain>
+            <Footer />
+          </Wrapper>
+        </ThemeRegistry>
       </body>
     </html>
   );
--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,9 +1,25 @@
-const Navbar = () => {
-  return (
-    <div>
-      <p>Navbar</p>
-    </div>
-  );
-};
+'use client';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default Navbar;
+export default function DenseAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Shopping
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
